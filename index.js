834b54674002c3fa2c630362533ea06cc7caec71
root <root@DESKTOP-T7KLEOV.localdomain>
2022-07-25 22:45:05 +0300

    completed 13.4

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const app = express();
 
 const Blog = require("./models/blog");
 
+app.use(express.json());
+
 app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  console.log(req.body);
+  const blog = await Blog.create({ ...req.body });
+  return res.json(blog);
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  console.log("executed");
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    return res.json(blog);
+  }
 });
 
 const PORT = process.env.PORT || 3001;
